0b1cba2d2a8da0885e62a13059b6e414a5ee997f
JonShelley <jshelley.menan@gmail.com>
2023-12-08 09:21:30 -0700

    updated pattern to match ubunt error message

--- oci/nw_checks/oci_rdma_link_flapping.sh
+++ oci/nw_checks/oci_rdma_link_flapping.sh
@@ -13,6 +13,7 @@ import socket
 RDMA_FLAPPING_LINK_TEST = "RDMA link failures detected"
 INTERFACES = ["enp12s0f0", "enp12s0f1", "enp42s0f0", "enp42s0f1", "enp65s0f0", "enp65s0f1", "enp88s0f0", "enp88s0f1", "enp134s0f0", "enp134s0f1", "enp165s0f0", "enp165s0f1", "enp189s0f0", "enp189s0f1", "enp213s0f0", "enp213s0f1"]
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Process RDMA link flapping data")
     parser.add_argument("-l", "--log-level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], default="INFO", help="Set the logging level")
@@ -34,7 +35,8 @@ def die(exit_code, message):
     sys.exit(exit_code)
 
 def get_rdma_link_failures(log_file):
-    pattern = r"(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+\S+\s+wpa_supplicant: (\w+): CTRL-EVENT-EAP-FAILURE EAP authentication failed"
+
+    pattern = r"(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+\S+\s+wpa_supplicant(?:\[\d+\])?: (\w+): CTRL-EVENT-EAP-FAILURE EAP authentication failed"
 
     link_data = {}
     with open(log_file, "r") as f:
