5dde777ebd24a532c162be5ac5e1085fad1111a0
JonShelley <jshelley.menan@gmail.com>
2023-12-08 12:11:31 -0700

    updated pattern to match ubunt error message

--- oci/nw_checks/oci_rdma_link_flapping.sh
+++ oci/nw_checks/oci_rdma_link_flapping.sh
@@ -39,6 +39,8 @@ def get_rdma_link_failures(log_file):
     pattern = r"(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+\S+\s+wpa_supplicant(?:\[\d+\])?: (\w+): CTRL-EVENT-EAP-FAILURE EAP authentication failed"
 
     link_data = {}
+    if log_file == "/var/log/syslog":
+        pattern2 = r"(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+\S+\s+kernel: \[\d+\.\d+\] mlx5_core \S+ (\w+): Link down"
     with open(log_file, "r") as f:
         for line in f:
             match = re.search(pattern, line)
@@ -47,11 +49,22 @@ def get_rdma_link_failures(log_file):
                 interface = match.group(2)
                 logging.debug(f"time: {time_str}, interface: {interface}")
                 if interface not in link_data:
-                    link_data[interface] = {"failures": [time_str]}
+                    link_data[interface] = {"failures": [time_str], "link_down": []}
                 else:
                     link_data[interface]["failures"].append(time_str)
+
+            if log_file == "/var/log/syslog":
+                match = re.search(pattern2, line)
+                if match:
+                    time_str = match.group(1)
+                    interface = match.group(2)
+                    logging.debug(f"time: {time_str}, interface: {interface}")
+                    if interface not in link_data:
+                        link_data[interface] = {"failures": [], "link_down": [time_str]}
+                    else:
+                        link_data[interface]["link_down"].append(time_str)
+                        
     logging.info("Link Data: {}".format(link_data))
-    #logging.info("Link Data: {}".format(link_data.keys()))
     return link_data
 
 def process_rdma_link_flapping(link_data, time_interval_hours):
@@ -60,24 +73,45 @@ def process_rdma_link_flapping(link_data, time_interval_hours):
         for interface in link_data:
             logging.info(f"{len(link_data[interface]['failures'])} RDMA link failure entries in /var/log/messages for interface: {interface}")
             
-            last_date_str = link_data[interface]["failures"][-1]
             current_date = datetime.datetime.now()
             current_date_str = current_date.strftime("%b %d %H:%M:%S")
+            
+            last_date_failure_str = None
+            last_date_down_str = None
 
-            last_date_sec = int(time.mktime(datetime.datetime.strptime(last_date_str, "%b %d %H:%M:%S").timetuple()))
+            if len(link_data[interface]["failures"]) > 0:
+                last_date_failure_str = link_data[interface]["failures"][-1]
+                last_date_failure_sec = int(time.mktime(datetime.datetime.strptime(last_date_failure_str, "%b %d %H:%M:%S").timetuple()))
+            
+            if len(link_data[interface]["link_down"]) > 0:
+                last_date_down_str = link_data[interface]["link_down"][-1]
+                last_date_down_sec = int(time.mktime(datetime.datetime.strptime(last_date_down_str, "%b %d %H:%M:%S").timetuple()))
+
+            
             current_date_sec = int(time.mktime(datetime.datetime.strptime(current_date_str, "%b %d %H:%M:%S").timetuple()))
             
-            if last_date_str != current_date_str:
-                diff_secs = current_date_sec - last_date_sec
+            if last_date_failure_str != None and last_date_failure_str != current_date_str:
+                diff_secs = current_date_sec - last_date_failure_sec
                 logging.info(f"RDMA link ({interface}) failed {diff_secs} seconds ago")
                 diff_hours = diff_secs // (60 * 60)
                 logging.info(f"RDMA link ({interface}) failed  {diff_hours} hours ago")
 
                 if diff_hours < time_interval_hours:
-                    logging.error(f"{RDMA_FLAPPING_LINK_TEST}, multiple RDMA link flapping events within {time_interval_hours} hours ({current_date_str}, {last_date_str})")
+                    logging.error(f"{RDMA_FLAPPING_LINK_TEST}, multiple RDMA link flapping events within {time_interval_hours} hours ({current_date_str}, {last_date_failure_str})")
                     status = -1
+            if last_date_down_str != None and last_date_down_str != current_date_str:
+                diff_secs = current_date_sec - last_date_down_sec
+                logging.info(f"RDMA link ({interface}) down {diff_secs} seconds ago")
+                diff_hours = diff_secs // (60 * 60)
+                logging.info(f"RDMA link ({interface}) down  {diff_hours} hours ago")
+
+                if diff_hours < time_interval_hours:
+                    logging.error(f"{RDMA_FLAPPING_LINK_TEST}, multiple RDMA link down events within {time_interval_hours} hours ({current_date_str}, {last_date_down_str})")
+                    status = -2
         if status == -1:
             die(-1, f"{process_rdma_link_flapping.__name__}: {RDMA_FLAPPING_LINK_TEST}, multiple RDMA link flapping events within the past {time_interval_hours} hours")
+        elif status == -2:
+            die(-2, f"{process_rdma_link_flapping.__name__}: {RDMA_FLAPPING_LINK_TEST}, multiple RDMA link down events within the past {time_interval_hours} hours")
     else:
         logging.info("No RDMA link failures entry in /var/log/messages")
         return 0
@@ -88,6 +122,8 @@ if __name__ == "__main__":
     setup_logging(args.log_level)
     auth_failure_file = "/tmp/last_auth_failure_date"
     msg_file = "/var/log/messages"
-    time_interval_hours = 12 
+    if not os.path.exists(msg_file):
+        msg_file = "/var/log/syslog"
+    time_interval_hours = 6
     link_data = get_rdma_link_failures(msg_file)
     process_rdma_link_flapping(link_data, time_interval_hours)
