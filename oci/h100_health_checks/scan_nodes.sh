f8d51bbb55a4336a1995dad1d71ba50c4839a11e
JonShelley <jshelley.menan@gmail.com>
2023-12-18 13:02:38 -0700

    fixed issue

--- oci/h100_health_checks/scan_nodes.sh
+++ oci/h100_health_checks/scan_nodes.sh
@@ -6,5 +6,6 @@ for x in `cat $HOSTLIST`
 do
     echo $x
     HID=$(ssh $x sudo /usr/sbin/dmidecode -s system-serial-number)
-    ssh $x "cd jshelley/cloud_scripts/oci/h100_health_checks;sudo python3 check_h100_setup.py | tee nhc_${HID}_\$HOSTNAME_2.out"
+    ssh $x "cd $BASEDIR;sudo python3 check_h100_setup.py 2>$1 | tee nhc_${HID}_\${HOSTNAME}.out"
+    scp $x:$BASEDIR/nhc_${HID}_\${HOSTNAME}.out .
 done
