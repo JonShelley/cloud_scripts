2f77831e17e1243f0140b45fa52357f186307833
JonShelley <jshelley.menan@gmail.com>
2023-12-18 13:06:23 -0700

    fixed issue

--- oci/h100_health_checks/scan_nodes.sh
+++ oci/h100_health_checks/scan_nodes.sh
@@ -2,10 +2,12 @@
 
 HOSTLIST=${1:-hostlist.txt}
 BASEDIR=${2:-~/cloud_scripts/oci/h100_health_checks}
+DATE=`date "+%Y%m%d-%H%M%S"`
+mkdir $DATE
 for x in `cat $HOSTLIST`
 do
     echo $x
     HID=$(ssh $x sudo /usr/sbin/dmidecode -s system-serial-number)
     ssh $x "cd $BASEDIR;sudo python3 check_h100_setup.py 2>$1 | tee nhc_${HID}_\${HOSTNAME}.out"
-    scp $x:$BASEDIR/nhc_${HID}_\${HOSTNAME}.out .
+    scp $x:$BASEDIR/nhc_${HID}_\${HOSTNAME}.out $DATE/.
 done
